13a0ec8958d2fbdf8c2958c21896f89f5354bf4f
carson <yuzhanwei@aliyun.com>
2019-07-05 22:30:08 +0800

    改为测试环境变量

--- src/config/env.js
+++ src/config/env.js
@@ -8,14 +8,17 @@ let websocket_iv = ''
 let http_api = 'http://127.0.0.1:8081'
 
 if (process.env.NODE_ENV === 'development') {
-    websocket_addr = 'ws://127.0.0.1:9111'
+    websocket_addr = 'ws://45.40.202.228:8080'
+    // websocket_addr = 'ws://127.0.0.1:9111'
     websocket_key = '700040D41F47592C'
     websocket_iv = '7AF3C134BEAA99E5'
 } else if (process.env.NODE_ENV === 'production') {
     // websocket_addr = 'ws://192.168.30.51:9111'
+    // websocket_key = 'A89226D089EE57FA'
+    // websocket_iv = '57EF7B0E6E582178'
     websocket_addr = 'ws://45.40.202.228:8080'
-    websocket_key = 'A89226D089EE57FA'
-    websocket_iv = '57EF7B0E6E582178'
+    websocket_key = '700040D41F47592C'
+    websocket_iv = '7AF3C134BEAA99E5'
 }
 
 export {
