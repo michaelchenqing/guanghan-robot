e154e7d57bcc9134f4e9ca56c596a75139337996
carson <yuzhanwei@aliyun.com>
2019-09-22 15:03:56 +0800

    部署线上环境

--- src/config/env.js
+++ src/config/env.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === 'development') {
     // websocket_addr = 'ws://192.168.30.51:9111'
     // websocket_key = 'A89226D089EE57FA'
     // websocket_iv = '57EF7B0E6E582178'
-    websocket_addr = 'ws://45.40.202.228:8080'
+    websocket_addr = 'ws://192.168.30.51:8080'
     websocket_key = '700040D41F47592C'
     websocket_iv = '7AF3C134BEAA99E5'
 }
