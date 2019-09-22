721fc17f49800dee84b0ce2aac367930bb996a63
carson <yuzhanwei@aliyun.com>
2019-09-22 18:10:18 +0800

    改为在线环境

--- src/config/env.js
+++ src/config/env.js
@@ -17,8 +17,8 @@ if (process.env.NODE_ENV === 'development') {
     // websocket_key = 'A89226D089EE57FA'
     // websocket_iv = '57EF7B0E6E582178'
     websocket_addr = 'ws://192.168.30.51:8080'
-    websocket_key = '700040D41F47592C'
-    websocket_iv = '7AF3C134BEAA99E5'
+    websocket_key = 'A89226D089EE57FA'
+    websocket_iv = '57EF7B0E6E582178'
 }
 
 export {
